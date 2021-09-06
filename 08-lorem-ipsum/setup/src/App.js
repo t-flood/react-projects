3e92409b99fbb09a7c230ed4eea751518ca4e464
Tim Flood <tflood9@gmail.com>
2021-09-06 11:45:19 -0400

    finish lorem ipsum project

--- 08-lorem-ipsum/setup/src/App.js
+++ 08-lorem-ipsum/setup/src/App.js
@@ -6,7 +6,15 @@ function App() {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log("hello world");
+    let amount = parseInt(count);
+    if (count <= 0) {
+      amount = 1;
+    }
+    if (count > 8) {
+      amount = 8;
+    }
+
+    setText(data.slice(0, amount));
   };
 
   return (
@@ -26,7 +34,9 @@ function App() {
         </button>
       </form>
       <article className="lorem-text">
-        <p>lorem ipsum</p>
+        {text.map((item, index) => {
+          return <p key={index}>{item}</p>;
+        })}
       </article>
     </section>
   );
